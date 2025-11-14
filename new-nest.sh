d6a25b8bfdd54c0a88fb32463ce64f1638b43a22
Sang Dang <sang@dang.to>
2025-11-14 17:07:25 +0200

    Fix escape character in PrismaService methods in new-nest.sh
    - Updated the onModuleInit and onModuleDestroy methods to correctly escape the $ character in the PrismaService class.
    - Ensured proper functionality of the Prisma connection and disconnection processes.

--- new-nest.sh
+++ new-nest.sh
@@ -113,11 +113,11 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
-    await this.$connect();
+    await this.\$connect();
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.\$disconnect();
   }
 }
 EOL
